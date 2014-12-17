901e4cbf4a442a5a97d4054a768da0066d9bd9f2
Tom Blackmore <tablackmore@gmail.com>
2014-12-17 11:48:57 +0100

    add clubs sql

---
+++ lectures/week1/day3-creating-tables-relations/exercise/clubs.sql
@@ -0,0 +1,194 @@
+DROP TABLE IF EXISTS `clubs`;
+
+CREATE TABLE `clubs` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `club_name` varchar(255),
+  `telephone` varchar(100) default NULL,
+  `street_address` varchar(255) default NULL,
+  `town` varchar(255),
+  `postcode` varchar(10) default NULL,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+
+
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Ac Mattis Ornare Industries","070 6178 9069","3519 A Rd.","Urbe","YN1L 4YA");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Cum Sociis Natoque Corp.","076 9160 5957","P.O. Box 451, 4018 Dolor. Ave","Tintigny","V5M 5UW");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Tincidunt PC","(0118) 001 8755","Ap #947-5442 Metus Street","Forge-Philippe","DD49 0GQ");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Mi Aliquam Foundation","0800 987 3261","2296 Elit. Rd.","Colchester","GP2S 8GA");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Orci Consectetuer Euismod LLC","(0114) 603 0525","P.O. Box 605, 2086 Consequat Street","Sennariolo","CL8J 8BB");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Vel Consulting","(016977) 2841","Ap #801-5649 Placerat St.","Saint-Laurent","EH7 8FW");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Convallis Est Inc.","07624 597575","Ap #870-4243 Donec Avenue","Sterrebeek","I0X 3LG");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Vitae Semper Egestas Corporation","0344 294 3998","443-5606 Congue, St.","Moe","P79 8LT");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Pede Et Risus LLC","0800 1111","P.O. Box 818, 1357 Proin Road","Aberystwyth","G9L 7BS");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Nec Mauris Blandit Corp.","(0118) 760 2301","Ap #321-3462 Vitae Rd.","Stokrooie","LU2 8UN");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Nec Luctus Felis Corp.","0500 200919","2171 Enim. Rd.","Honolulu","JG6Z 6EO");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Dapibus Rutrum LLC","(016977) 1781","529-8704 Purus. Road","Montpelier","D9 5LE");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Metus Vitae Velit Ltd","0373 494 6550","4651 Dolor Street","Mildura","B7 5YY");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Sed Leo Cras Company","0500 447578","793-5422 Augue St.","Castelnuovo Magra","M44 2TT");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Nec Imperdiet Nec Institute","07080 703107","731-6047 Iaculis Rd.","Asso","G9B 9QV");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Enim Nisl Elementum Foundation","(01205) 322642","P.O. Box 696, 1366 Eu Street","Meerdonk","LV7 8RA");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Sed Incorporated","0382 898 0651","Ap #980-7174 Ac Road","Cargovil","R1N 1TC");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Ut Inc.","070 0831 3995","849 Cursus, Street","Chastre-Villeroux-Blanmont","EI02 9MP");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Varius Orci In Industries","0348 630 2445","776-2646 Orci Rd.","Laces/Latsch","VA3 8IB");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Orci Sem Eget Company","0800 1111","P.O. Box 968, 8426 Vestibulum Street","Trois-Rivi?res","XG4 1ST");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Vitae Sodales At Industries","07624 668843","7339 Dolor Rd.","Gander","VI42 2VW");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Duis Cursus Diam Corp.","(01971) 976272","620-9250 Nunc. St.","Columbia","H2T 4TR");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Pellentesque Habitant Limited","(01932) 62119","Ap #377-7920 Ligula. St.","Inverbervie","Y0I 1MI");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Mauris Ut LLP","0343 313 2793","Ap #413-640 Nisi. St.","Mainz","O4S 5LC");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Fusce Fermentum Corporation","056 3880 9228","Ap #420-5777 Eu, Road","Stoke-on-Trent","BU37 3YG");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Nibh Institute","(0121) 936 3821","6567 Ac Street","La Matapï¿½dia","J47 5JE");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Nulla Magna Incorporated","056 0955 1339","P.O. Box 997, 4819 Mauris. St.","Pike Creek","L6C 6GG");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Nunc Incorporated","076 2413 0500","107-3919 Magnis Ave","Annone di Brianza","C2V 5RJ");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Lacinia Corporation","(016977) 3780","P.O. Box 114, 2648 Nec Avenue","Aubange","HG9B 9DT");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Risus Industries","(01862) 786459","P.O. Box 577, 1508 Vel, Road","Chambord","O9G 8QN");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Arcu Morbi Sit LLC","0845 46 42","319-6348 Ultricies Road","Bremen","AK8 2VB");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Et Nunc PC","0375 826 5841","218-3885 Amet Ave","Chandler","KT49 6GK");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Lacus LLP","076 0267 0630","Ap #929-2119 Dolor. Av.","Lacombe County","OF4T 1OW");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Nulla Interdum Curabitur Incorporated","(024) 1775 6450","367-1276 Dictum Av.","Birmingham","DU21 2AA");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Eget Varius Ultrices Consulting","(01523) 12688","152-5007 Eu, Road","Arlon","XQ2 2VJ");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Felis Ltd","(021) 6051 7242","247-3888 Donec Rd.","Challand-Saint-Victor","Z8 8KK");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Fermentum Fermentum Arcu Inc.","07456 509402","391-6023 Quis, Street","Des Moines","Z4 2GG");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("A Enim Suspendisse Incorporated","0353 305 8055","Ap #932-5794 At, St.","Norfolk County","Y1K 6QJ");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Augue Eu Tempor LLP","0800 549 7841","P.O. Box 551, 5149 Duis Avenue","Kapelle-op-den-Bos","GN54 2OE");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Lacinia At PC","070 8835 0016","Ap #512-8990 Augue Street","Phoenix","NM7 2VR");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Pede LLC","0887 781 7443","Ap #336-1054 Massa. St.","Monacilioni","CQ74 3QG");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Lobortis Mauris Limited","(015552) 89398","6352 In Street","La Salle","E50 5LG");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Venenatis Company","0870 340 4847","2318 Laoreet St.","Geesthacht","SL5 8EM");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Risus Corporation","0800 370243","369-365 Dis St.","Armstrong","B5A 4BI");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Erat Limited","0854 233 7502","992-4620 Turpis. Rd.","Comox","KE8N 9JX");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Sed Inc.","0870 026 9311","Ap #230-397 Erat Rd.","Banff","D8S 7BT");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Tristique Corporation","(016977) 9561","991-7939 Non Avenue","Kaneohe","I68 7ND");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Dui Inc.","056 1369 0666","587-443 Curabitur Street","Groï¿½-Gerau","FN25 7CV");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Non Incorporated","0305 159 3038","507-4344 Risus. Ave","Wolfenbï¿½ttel","TC9 6UA");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Penatibus Et Ltd","(0110) 147 4556","1853 Pharetra Road","Vlezenbeek","P0 4MU");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Nullam Nisl Associates","(01071) 42467","486-2447 Congue, Street","Poitiers","VV9S 1RA");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Elit PC","0800 046200","386-5411 Elementum, Rd.","Cambiago","G7S 4JC");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Non Magna Nam PC","0800 707380","P.O. Box 688, 7260 Ut Av.","Christchurch","UD4L 5CG");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Orci Donec Limited","0394 396 2037","P.O. Box 732, 4973 Vel St.","Sibret","MO2 4UH");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Vitae Semper Egestas Foundation","(0111) 576 0940","P.O. Box 680, 5331 Ipsum Rd.","Grembergen","UZ31 1SP");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Sed Inc.","(0119) 567 3461","3188 Odio Av.","Lieferinge","E50 1NI");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Iaculis Enim Sit Company","(01697) 267719","P.O. Box 873, 3175 Fusce Ave","Southaven","XQ32 5JD");
+INSERT INTO `clubs` (`club_name`,`telephone`,`street_address`,`town`,`postcode`) VALUES ("Molestie Foundation","(016977) 5664","488-8179 Vel Road","Wanfercï¿½e-Baulet","FC43 1JK");
+
+DROP TABLE IF EXISTS `staff_clubs`;
+
+CREATE TABLE IF NOT EXISTS `floggit`.`staff_clubs` (
+  `staff_id` MEDIUMINT(8) UNSIGNED NOT NULL,
+  `club_id` MEDIUMINT(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`staff_id`, `club_id`),
+  INDEX `fk_staff_has_clubs_clubs1_idx` (`club_id` ASC),
+  INDEX `fk_staff_has_clubs_staff1_idx` (`staff_id` ASC),
+  CONSTRAINT `fk_staff_has_clubs_staff1`
+    FOREIGN KEY (`staff_id`)
+    REFERENCES `floggit`.`staff` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_staff_has_clubs_clubs1`
+    FOREIGN KEY (`club_id`)
+    REFERENCES `floggit`.`clubs` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (58,100);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (1,6);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (47,66);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (50,43);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (32,8);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (13,28);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (46,44);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (47,64);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (57,95);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (58,32);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (46,14);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (47,14);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (40,64);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (1,93);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (58,57);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (51,73);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (48,11);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (5,13);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (22,27);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (22,78);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (7,27);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (41,26);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (14,21);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (8,90);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (32,6);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (58,83);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (15,71);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (3,44);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (7,76);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (52,71);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (54,73);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (20,78);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (25,20);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (53,12);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (40,15);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (6,90);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (11,84);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (46,100);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (51,59);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (23,31);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (4,59);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (13,8);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (23,91);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (51,35);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (12,43);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (5,58);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (53,27);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (51,37);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (20,7);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (57,66);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (57,30);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (23,14);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (49,100);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (3,63);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (28,48);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (10,21);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (59,12);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (1,62);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (19,6);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (11,89);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (50,19);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (47,15);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (40,5);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (16,27);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (30,57);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (10,37);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (16,38);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (12,15);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (27,30);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (13,9);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (10,42);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (1,46);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (42,75);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (56,66);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (5,49);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (1,36);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (42,86);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (16,86);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (23,46);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (33,10);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (12,41);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (54,54);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (11,62);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (51,27);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (29,64);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (9,33);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (19,70);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (32,16);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (23,64);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (45,27);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (15,81);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (31,9);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (30,33);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (8,21);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (59,74);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (7,72);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (35,88);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (26,78);
+INSERT INTO staff_clubs (club_id,staff_id) VALUES (35,38);
